0522f7f17badeda15a0b18f4f9157a373c9c2752
Alejandro Lucena <ajl18@rice.edu>
2026-02-06 15:46:28 -0600

    report + updated .sh file

--- exp1_dgemm/scripts/measure.sh
+++ exp1_dgemm/scripts/measure.sh
@@ -13,7 +13,10 @@ for n in "${SIZES[@]}"; do
   for impl in "${IMPLS[@]}"; do
     echo "Running $impl N=$n"
     # TODO(student): parse binary output and append to CSV (e.g., using grep/awk)
-    "$BIN" --m "$n" --n "$n" --k "$n" --impl "$impl" --no-verify
+    # 2>&1 ensures that the binary prints is caught even if it sent to stderr
+    "$BIN" --m "$n" --n "$n" --k "$n" --impl "$impl" --no-verify 2>&1 \
+      | grep "Impl=" \
+      | awk -F'[= ]' '{print $2","$4","$6","$8","$10","$12}' >> "$LOG"
   done
 done
 
