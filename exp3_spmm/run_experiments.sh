9309f19f26c0372d0924092537a65aa754e6aec5
Alejandro Lucena <ajl18@rice.edu>
2026-02-27 16:37:24 -0600

    final corrected script for opt

--- exp3_spmm/run_experiments.sh
+++ exp3_spmm/run_experiments.sh
@@ -5,5 +5,5 @@ echo "density,gflops" > plot_data.csv
 # Run for different densities
 for d in 0.01 0.02 0.04 0.06 0.08 0.10; do
     echo -n "$d," >> plot_data.csv
-    ./spmm_opt $d | grep "Throughput" | awk '{print $1}' >> plot_data.csv
+    ./spmm_opt $d | grep "Throughput" | awk '{print $6}' >> plot_data.csv
 done
