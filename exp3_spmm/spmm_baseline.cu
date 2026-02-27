846b8174173746ef1e5d766d7ed3cf5109295769
Alejandro Lucena <ajl18@rice.edu>
2026-02-27 12:48:53 -0600

    removed RAND from baseline

--- exp3_spmm/spmm_baseline.cu
+++ exp3_spmm/spmm_baseline.cu
@@ -118,7 +118,13 @@ int main(int argc, char** argv) {
 
     // Create B
     std::vector<float> B((size_t)K * N);
-    for (size_t i = 0; i < B.size(); i++) B[i] = float(rand()) / RAND_MAX;
+    //Original loop
+    //for (size_t i = 0; i < B.size(); i++) B[i] = float(rand()) / RAND_MAX;
+    std::mt19937 b_gen(5678); // Use a DIFFERENT fixed seed than the CSR matrix
+    std::uniform_real_distribution<float> dist(0.0f, 1.0f);
+    for (size_t i = 0; i < B.size(); i++) {
+        B[i] = dist(b_gen);
+    }
 
     // CPU reference
     std::vector<float> C_ref;
