56dd65fa082c89c914d464aeec6ac1e2970c6686
Alejandro Lucena <ajl18@rice.edu>
2026-02-27 16:32:09 -0600

    testing different densities in script

--- exp3_spmm/run_experiments.sh
+++ exp3_spmm/run_experiments.sh
@@ -1,5 +1,9 @@
 
 #!/bin/bash
-# Example run script
-./spmm_baseline
-./spmm_opt
+echo "density,gflops" > plot_data.csv
+
+# Run for different densities
+for d in 0.01 0.02 0.04 0.06 0.08 0.10; do
+    echo -n "$d," >> plot_data.csv
+    ./spmm_opt $d | grep "Throughput" | awk '{print $2}' >> plot_data.csv
+done
