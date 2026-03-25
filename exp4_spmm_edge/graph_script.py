91e57c606dd5a59f84d23da4177d8d5bf988683f
Alejandro Lucena <ajl18@rice.edu>
2026-03-25 16:48:54 -0500

    graph script for 200 nodes

---
+++ exp4_spmm_edge/graph_script.py
@@ -0,0 +1,12 @@
+import random
+
+M = 2000  # Number of nodes
+Avg_Edges = 30 
+
+with open("graph_edges.txt", "w") as f:
+    for u in range(M):
+        # Give each node a random number of neighbors
+        neighbors = random.sample(range(M), Avg_Edges)
+        for v in neighbors:
+            if u != v:
+                f.write(f"{u} {v}\n")
