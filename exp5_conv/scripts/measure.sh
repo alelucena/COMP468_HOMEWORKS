dd37d8d5ac18105b57b0ec059933a88f0ee6236e
Alejandro Lucena <ajl18@rice.edu>
2026-03-31 13:31:43 -0500

    WORKING on paramter sweep script

--- exp5_conv/scripts/measure.sh
+++ exp5_conv/scripts/measure.sh
@@ -20,7 +20,8 @@ for h in "${SPATIAL[@]}"; do
       echo "Running $impl H=$h Cout=$cout"
       # TODO(student): parse stdout from the binary and append to the CSV.
       "$BIN" --height "$h" --width "$h" --channels "$CIN" --filters "$cout" \
-        --ksize "$K" --stride "$STRIDE" --padding "$PADDING" --impl "$impl" --no-verify
+      --ksize "$K" --stride "$STRIDE" --padding "$PADDING" --impl "$impl" --no-verify 2>&1 \ 
+      | grep "Impl=" \ awk '{print $0}' >> "$LOG"
     done
   done
 done
